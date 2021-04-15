d6e79077f88681ad0ae4d954cc05b2c45bfe4352
Saoud <saoud@saoud.dev>
2021-04-14 20:38:46 -0700

    adding logic for answer but it is commented out, since it doesn't work quite yet, just a proof of concept
    Co-authored-by: Shannon Lee <shannonleehj@gmail.com>

--- Allergies.Tests/ModelTests/AllergiesTests.cs
+++ Allergies.Tests/ModelTests/AllergiesTests.cs
@@ -1,6 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
 using Allergies.Models;
+using System.Linq;
 
 namespace Allergies.Tests
 {
--- Allergies/Models/Allergies.cs
+++ Allergies/Models/Allergies.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Allergies.Models
 {
@@ -21,3 +22,52 @@ namespace Allergies.Models
       }
     }
 }
+
+  //  public string GetAllergyScore(int score)
+  //   {
+  //     if (score < 1)
+  //     {
+  //       return string.Empty;
+  //     }
+  //     else if (score > 255)
+  //     {
+  //       return "error: please enter valid number less than 256.";
+  //     }
+  //     else if (score >= 128)
+  //     {
+  //       return "cats " + GetAllergyScore(score - 128);
+  //     }
+  //     else if (score >= 64)
+  //     {
+  //       return "pollen " + GetAllergyScore(score - 64);
+  //     }
+  //     else if (score >= 32)
+  //     {
+  //       return "chocolate " + GetAllergyScore(score - 32);
+  //     }
+  //     else if (score >= 16)
+  //     {
+  //       return "tomatoes " + GetAllergyScore(score - 16);
+  //     }
+  //     else if (score >= 8)
+  //     {
+  //       return "strawberries " + GetAllergyScore(score - 8);
+  //     }
+  //     else if (score >= 4)
+  //     {
+  //       return "shellfish " + GetAllergyScore(score - 4);
+  //     }
+  //     else if (score >= 2)
+  //     {
+  //       return "peanuts " + GetAllergyScore(score - 2);
+  //     }
+  //     else
+  //     {
+  //       return "eggs.";
+  //     }
+  //   }
+
+
+// allergy = (1 + 2 ^ x)
+// where x = element of array
+// [0,1,2,3,4,5,6]
