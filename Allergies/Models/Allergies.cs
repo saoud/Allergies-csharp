e6f80389e2d32b1466556977cb6a5d7c380e6ec6
shanole <73411129+shanole@users.noreply.github.com>
2021-04-14 22:59:09 -0700

    make fields private and delete commented out code

--- Allergies/Models/Allergies.cs
+++ Allergies/Models/Allergies.cs
@@ -6,7 +6,7 @@ namespace Allergies.Models
 {
     public class AllergyScore
     {
-      public string[] Allergens = {"eggs","peanuts","shellfish","strawberries","tomatoes","chocolate","pollen","cats"};
+      private string[] _Allergens = {"eggs","peanuts","shellfish","strawberries","tomatoes","chocolate","pollen","cats"};
 
       public List<string> CheckAllergies(int userSum)
       {
@@ -18,7 +18,7 @@ namespace Allergies.Models
         {
           if (reverseBinary[i] == '1')
           {
-            allergiesList.Add(Allergens[i]);
+            allergiesList.Add(_Allergens[i]);
           }
         }
         return allergiesList;
@@ -32,52 +32,3 @@ namespace Allergies.Models
       }
     }
 }
-
-  //  public string GetAllergyScore(int score)
-  //   {
-  //     if (score < 1)
-  //     {
-  //       return string.Empty;
-  //     }
-  //     else if (score > 255)
-  //     {
-  //       return "error: please enter valid number less than 256.";
-  //     }
-  //     else if (score >= 128)
-  //     {
-  //       return "cats " + GetAllergyScore(score - 128);
-  //     }
-  //     else if (score >= 64)
-  //     {
-  //       return "pollen " + GetAllergyScore(score - 64);
-  //     }
-  //     else if (score >= 32)
-  //     {
-  //       return "chocolate " + GetAllergyScore(score - 32);
-  //     }
-  //     else if (score >= 16)
-  //     {
-  //       return "tomatoes " + GetAllergyScore(score - 16);
-  //     }
-  //     else if (score >= 8)
-  //     {
-  //       return "strawberries " + GetAllergyScore(score - 8);
-  //     }
-  //     else if (score >= 4)
-  //     {
-  //       return "shellfish " + GetAllergyScore(score - 4);
-  //     }
-  //     else if (score >= 2)
-  //     {
-  //       return "peanuts " + GetAllergyScore(score - 2);
-  //     }
-  //     else
-  //     {
-  //       return "eggs.";
-  //     }
-  //   }
-
-
-// allergy = (1 + 2 ^ x)
-// where x = element of array
-// [0,1,2,3,4,5,6]
